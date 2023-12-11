232725532097fdadeba54a181da52f7521988289
yinhuasheng <yinhuasheng@unicloud.com>
2023-12-11 19:22:20 +0800

    webflux

---
+++ web-flux-demo/src/main/java/com/coderzoe/WebFluxApplication.java
@@ -0,0 +1,16 @@
+package com.coderzoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yinhuasheng
+ * @email yinhuasheng@unicloud.com
+ * @date 2023/12/11 17:49
+ */
+@SpringBootApplication
+public class WebFluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class,args);
+    }
+}
